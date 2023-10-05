c02561eb2a1a572e956bc111912469a61d313bce
Muhammad Sami <muhammadsami1242@gmail.com>
2023-10-05 13:36:17 +0500

    changed

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+console.log("Welcome to Spotify");
+let audioElement = new Audio("songs/1.mp3");
+let progessbar = document.getElementById('myprogressbar').addEventListener('timeupdate' , function(){
+    var currentTime = audioElement.currentTime;
+    var duration = audioElement.duration;
+    var progressPercentage = (currentTime / duration) * 100;
+    progressBar.value = progressPercentage;
+});
+
+function runsong(){
+    audioElement.play();
+}
