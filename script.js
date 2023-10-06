4ebe72595d464fc7514637685d59a71992ea07f7
Muhammad Sami <muhammadsami1242@gmail.com>
2023-10-06 15:07:03 +0500

    changed

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 console.log("Welcome to Spotify");
-let audioElement = new Audio("songs/1.mp3");
-let progessbar = document.getElementById('myprogressbar').addEventListener('timeupdate' , function(){
-    var currentTime = audioElement.currentTime;
-    var duration = audioElement.duration;
-    var progressPercentage = (currentTime / duration) * 100;
-    progressBar.value = progressPercentage;
-});
 
-function runsong(){
-    audioElement.play();
-}
+document.addEventListener('DOMContentLoaded' , ()=>{
+
+                //Variables 
+let audioElement = new Audio("songs/1.mp3");
+let masterplay = document.getElementById('masterplay');
+let myprogressbar = document.getElementById('myprogressbar');
+
+ 
+                // Play/Pause click
+masterplay.addEventListener('click' , ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterplay.classList.remove('fa-circle-play');
+        masterplay.classList.add('fa-circle-pause');        
+    }
+    else {
+        audioElement.pause();
+        masterplay.classList.remove('fa-circle-pause'); 
+        masterplay.classList.add('fa-circle-play');
+    }
+})
+
+                // Progressbar
+myprogressbar.addEventListener('timeupdate' , ()=>{
+        console.log('timeupdate');
+})
+
+
+
+})
+
+
