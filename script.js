d8e5d91afe45fad2005f6d248e8c6c8d68bf0b4a
Muhammad Sami <muhammadsami1242@gmail.com>
2023-10-07 12:44:03 +0500

    changed

--- script.js
+++ script.js
@@ -6,25 +6,32 @@ document.addEventListener('DOMContentLoaded' , ()=>{
 let audioElement = new Audio("songs/1.mp3");
 let masterplay = document.getElementById('masterplay');
 let myprogressbar = document.getElementById('myprogressbar');
+let gif = document.getElementById('gif');
 
  
                 // Play/Pause click
 masterplay.addEventListener('click' , ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
+        gif.style.opacity = "1";
         masterplay.classList.remove('fa-circle-play');
         masterplay.classList.add('fa-circle-pause');        
     }
     else {
         audioElement.pause();
+        gif.style.opacity="0";
         masterplay.classList.remove('fa-circle-pause'); 
         masterplay.classList.add('fa-circle-play');
     }
 })
 
                 // Progressbar
-myprogressbar.addEventListener('timeupdate' , ()=>{
-        console.log('timeupdate');
+audioElement.addEventListener('timeupdate' , ()=>{
+    console.log('timeupdate');
+    // updata seekbar
+    progress =parseInt((audioElement.currentTime/audioElement.duration)*100);    
+    console.log('progress');
+    myprogressbar.value = progress; 
 })
 
 
