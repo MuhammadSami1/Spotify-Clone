b7e24c84061d5b52ee9384a6a447bcdd934e447e
Muhammad Sami <muhammadsami1242@gmail.com>
2023-10-08 13:29:57 +0500

    changed

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 console.log("Welcome to Spotify");
 
-document.addEventListener('DOMContentLoaded' , ()=>{
+document.addEventListener('DOMContentLoaded', () => 
+{
 
-                //Variables 
-let audioElement = new Audio("songs/1.mp3");
-let masterplay = document.getElementById('masterplay');
-let myprogressbar = document.getElementById('myprogressbar');
-let gif = document.getElementById('gif');
-
- 
-                // Play/Pause click
-masterplay.addEventListener('click' , ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        gif.style.opacity = "1";
-        masterplay.classList.remove('fa-circle-play');
-        masterplay.classList.add('fa-circle-pause');        
-    }
-    else {
-        audioElement.pause();
-        gif.style.opacity="0";
-        masterplay.classList.remove('fa-circle-pause'); 
-        masterplay.classList.add('fa-circle-play');
-    }
-})
-
-                // Progressbar
-audioElement.addEventListener('timeupdate' , ()=>{
-    console.log('timeupdate');
-    // updata seekbar
-    progress =parseInt((audioElement.currentTime/audioElement.duration)*100);    
-    console.log('progress');
-    myprogressbar.value = progress; 
-})
+    //Variables 
+    let audioElement = new Audio("songs/1.mp3");
+    let masterplay = document.getElementById('masterplay');
+    let myprogressbar = document.getElementById('myprogressbar');
+    let gif = document.getElementById('gif');
 
 
+    // Play/Pause click
+    masterplay.addEventListener('click', () => 
+    {
+        if (audioElement.paused || audioElement.currentTime <= 0) {
+            audioElement.play();
+            gif.style.opacity = "1";
+            masterplay.classList.remove('fa-circle-play');
+            masterplay.classList.add('fa-circle-pause');
+        }
+        else {
+            audioElement.pause();
+            gif.style.opacity = "0";
+            masterplay.classList.remove('fa-circle-pause');
+            masterplay.classList.add('fa-circle-play');
+        }
+    })
 
-})
+    // Progressbar
+    audioElement.addEventListener('timeupdate', () => 
+    {
+        // updata seekbar
+        progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+        myprogressbar.value = progress;
+    
+    });
+    
+    myprogressbar.addEventListener('change', () => 
+    {
+        audioElement.currentTime = myprogressbar.value * audioElement.duration / 100;
+    });
+
+
+
+
+});
 
 
